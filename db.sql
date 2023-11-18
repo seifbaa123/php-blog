59cdd72f1d98cc680a7197e8eb24fb31ca016a1d
seif <seifbaa123@gmail.com>
2023-11-18 10:39:28 +0100

    add database tables

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posts (
+    post_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255),
+    content TEXT,
+    username VARCHAR(255) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+);
