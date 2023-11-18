9ad2b8743acb34a9caa24a7ea553edf30cd13490
seif <seifbaa123@gmail.com>
2023-11-18 11:45:32 +0100

    fetch single post from db

--- lib/models/posts.php
+++ lib/models/posts.php
@@ -18,4 +18,20 @@ class Posts
             exit();
         }
     }
+
+    static function get_by_id($id)
+    {
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM posts WHERE post_id = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_CLASS);
+            return $results[0];
+        } catch (PDOException $e) {
+            header("Location: ./500.php");
+            exit();
+        }
+    }
 }
--- post.php
+++ post.php
@@ -1,46 +1,24 @@
 <?php
-include "./inc/header.php";
+
+require "./inc/header.php";
+require "./lib/models/posts.php";
+
+$post = Posts::get_by_id($_GET["id"]);
+
 ?>
 
 <main class="single-post">
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit</h1>
-    <img src="./images/house-3.jpg" alt="house" />
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        aperiam veritatis optio iusto non ex accusamus reprehenderit vero
-        distinctio deserunt quibusdam, in placeat id quia sint alias numquam
-        odio doloremque facere. Dicta enim sint distinctio repellendus
-        repudiandae maiores mollitia? Numquam optio ipsam illum ducimus porro
-        doloremque amet voluptatem cumque similique nihil. Necessitatibus esse
-        officia vitae deleniti exercitationem eligendi ab dicta voluptatem,
-        provident perferendis pariatur natus blanditiis nulla optio vel numquam
-        nihil hic tempore voluptatum molestias accusamus, adipisci facilis?
-        Distinctio voluptates quia labore, nemo sint minima quasi temporibus
-        nostrum enim deserunt, quisquam eaque sed deleniti cumque pariatur, sit
-        voluptas tempore ipsum.
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        aperiam veritatis optio iusto non ex accusamus reprehenderit vero
-        distinctio deserunt quibusdam, in placeat id quia sint alias numquam
-        odio doloremque facere. Dicta enim sint distinctio repellendus
-        repudiandae maiores mollitia? Numquam optio ipsam illum ducimus porro
-        doloremque amet voluptatem cumque similique nihil.
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        aperiam veritatis optio iusto non ex accusamus reprehenderit vero
-        distinctio deserunt quibusdam, in placeat id quia sint alias numquam
-        odio doloremque facere. Dicta enim sint distinctio repellendus
-        repudiandae maiores mollitia? Numquam optio ipsam illum ducimus porro
-        doloremque amet voluptatem cumque similique nihil. Necessitatibus esse
-        officia vitae deleniti exercitationem eligendi ab dicta voluptatem,
-        provident perferendis pariatur natus blanditiis nulla optio vel numquam
-        nihil hic tempore voluptatum molestias accusamus, adipisci facilis?
-        Distinctio voluptates quia labore, nemo sint minima quasi temporibus
-        nostrum enim deserunt, quisquam eaque sed deleniti cumque pariatur, sit
-        voluptas tempore ipsum.
-    </p>
+    <?php if ($post != null): ?>
+        <h1>
+            <?= $post->title ?>
+        </h1>
+        <img src="./images/<?= $post->image_url ?>" alt="<?= $post->title ?>" />
+        <p>
+            <?= $post->content ?>
+        </p>
+    <?php else: ?>
+        <h1>Post not found</h1>
+    <?php endif; ?>
 </main>
 
 <?php
