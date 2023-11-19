052766fa4938b4ff3b9328564f40093ce7ac56b0
seif <seifbaa123@gmail.com>
2023-11-19 08:47:15 +0100

    list posts on dashboard page

--- dashboard/index.php
+++ dashboard/index.php
@@ -5,6 +5,7 @@ require "../lib/auth.php";
 require "../inc/header.php";
 require "../lib/models/posts.php";
 
+$posts = Posts::get_all();
 
 ?>
 
@@ -15,6 +16,37 @@ require "../lib/models/posts.php";
             <button>Logout</button>
         </form>
     </header>
+
+    <div class="table">
+
+        <?php if (count($posts) == 0): ?>
+            <p style="text-align: center">There is no posts yet!</p>
+        <?php else: ?>
+            <table>
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <?php foreach ($posts as $post): ?>
+                        <tr>
+                            <td>
+                                <?= $post->id ?>
+                            </td>
+                            <td>
+                                <?= $post->title ?>
+                            </td>
+                            <td>
+                                <a class="link success" href="/dashboard/update?id=<?= $post->id ?>">update</a>
+                                <a class="link danger" href="/dashboard/delete?id=<?= $post->id ?>">delete</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
+    </div>
 </main>
 
 <?php
