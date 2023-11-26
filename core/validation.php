137571d67521b64145aa8631e7d9bea8c836e114
seif <seifbaa123@gmail.com>
2023-11-26 10:44:26 +0100

    add number validation

--- core/validation.php
+++ core/validation.php
@@ -3,15 +3,28 @@
 function validate($data, $rules) {
     foreach ($rules as $field => $field_rules) {
         foreach ($field_rules as $rule) {
-            switch ($rule) {
-                case "required":
-                    if (!isset($data[$field]) || $data[$field] == "") {
-                        return ucfirst($field) . " Must not be empty";
-                    }
-                    break;
-                default:
-                    throw new Exception("Invalid rule $rule");
+            if ($rule == "required") {
+                if (!isset($data[$field]) || $data[$field] == "") {
+                    return ucfirst($field) . " Must not be empty";
+                }
+                continue;
             }
+
+            if ($rule == "number") {
+                if (!isset($data[$field]) || !is_int(intval($data[$field]))) {
+                    return ucfirst($field) . " Must be a number";
+                }
+                continue;
+            }
+
+            if ($rule == "float") {
+                if (!isset($data[$field]) || !is_float(floatval($data[$field]))) {
+                    return ucfirst($field) . " Must be a float";
+                }
+                continue;
+            }
+
+            throw new Exception("Invalid rule $rule");
         }
     }
 
